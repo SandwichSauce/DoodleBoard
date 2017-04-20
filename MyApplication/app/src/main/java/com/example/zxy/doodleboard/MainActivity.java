83ca4f87e6a188e622561570853dfd030359f9bd
SandwichSauce <512079720@512079720.com>
2017-04-20 10:25:31 +0800

    ex3-v1.1
    精简程序，美化排版，完整注释。修复画板颜色bug。

--- MyApplication/app/src/main/java/com/example/zxy/doodleboard/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/zxy/doodleboard/MainActivity.java
@@ -2,16 +2,12 @@ package com.example.zxy.doodleboard;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
@@ -33,66 +29,73 @@ public class MainActivity extends Activity implements OnClickListener {
         initView();
     }
     private void initView() {
-        image=(ImageView) findViewById(R.id.image);
-        save=(Button) findViewById(R.id.save);
-        reset=(Button) findViewById(R.id.reset);
-        image.setOnTouchListener(new myOnTouchListener());
+        image = (ImageView) findViewById(R.id.image);
+        save = (Button) findViewById(R.id.save);
+        reset = (Button) findViewById(R.id.reset);
+        image.setOnTouchListener(new onTouchListener());
         save.setOnClickListener(this);
         reset.setOnClickListener(this);
     }
-    class myOnTouchListener implements OnTouchListener{
+    class onTouchListener implements OnTouchListener{
         @Override
-        public boolean onTouch(View arg0, MotionEvent arg1) {
-            int action=arg1.getAction();
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            int action = motionEvent.getAction();
             switch (action) {
-                case MotionEvent.ACTION_DOWN://按下触发
+                //按下触发
+                case MotionEvent.ACTION_DOWN:
                     System.out.println("按下");
                     initPanel();
-                    downX=arg1.getX();
-                    downY=arg1.getY();
+                    downX=motionEvent.getX();
+                    downY=motionEvent.getY();
                     break;
-                case MotionEvent.ACTION_MOVE://移动触发
+                //移动触发
+                case MotionEvent.ACTION_MOVE:
                     System.out.println("移动");
-                    float moveX= arg1.getX();
-                    float moveY=arg1.getY();
+                    float moveX = motionEvent.getX();
+                    float moveY = motionEvent.getY();
                     canvas.drawLine(downX, downY, moveX, moveY, paint);
                     image.setImageBitmap(panel);
-                    downX=moveX;
-                    downY=moveY;
+                    downX = moveX;
+                    downY = moveY;
                     break;
-                case MotionEvent.ACTION_UP://松开触发
+                //松开触发
+                case MotionEvent.ACTION_UP:
                     System.out.println("松开");
                     break;
                 default:
                     break;
             }
-            return true;//false只触发按下  true全触发
+            //false只触发按下  true全触发
+            return true;
         }
         private void initPanel() {
-            if(panel==null){
-                panel=Bitmap.createBitmap(image.getWidth(), image.getHeight(), Config.ARGB_8888);//画纸
-//初始化一个画板
-                canvas=new Canvas(panel);
-//初始化画笔
-                paint=new Paint();
-                paint.setColor(Color.RED);//指定颜色
-                paint.setStrokeWidth(5);//指定宽度
+            if(panel == null){
+                panel = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Config.ARGB_8888);//画纸
+                //初始化一个画板
+                canvas = new Canvas(panel);
+                //初始化画笔
+                paint = new Paint();
+                //指定画笔颜色
+                paint.setColor(Color.RED);
+                //指定宽度
+                paint.setStrokeWidth(5);
+                //指定画板颜色
                 canvas.drawColor(Color.YELLOW);
                 image.setImageBitmap(panel);
             }
         }
     }
     @Override
-    public void onClick(View arg0) {
-        int id=arg0.getId();
+    public void onClick(View view) {
+        int id = view.getId();
         System.out.println("id"+id);
-        if(id==R.id.save){
+        if(id == R.id.save){
             System.out.println("保存...");
-//保存
-        }else if(id==R.id.reset){
-//取消
+        //保存
+        }else if(id == R.id.reset){
+            //清空
             System.out.println("取消...");
-            canvas.drawColor(Color.WHITE);
+            canvas.drawColor(Color.YELLOW);
             image.setImageBitmap(panel);
         }
     }
